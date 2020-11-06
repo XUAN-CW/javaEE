a2f4e1a8180309a9d4b2cf9a9ff93b00d01541b9
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-06 16:55:15 +0800

    完成了实验三

---
+++ lab03/consumer/consumer/src/main/java/edu/guet/controller/MathController.java
@@ -0,0 +1,11 @@
+package edu.guet.controller;
+
+/**
+ * @author XUAN
+ * @date 2020/10/23 - 11:13
+ * @references
+ * @purpose
+ * @errors
+ */
+public class MathController {
+}
---
+++ lab03/consumer/consumer/src/main/java/edu/guet/service/MathService.java
@@ -0,0 +1,12 @@
+package edu.guet.service;
+
+/**
+ * @author XUAN
+ * @date 2020/10/23 - 11:11
+ * @references
+ * @purpose
+ * @errors
+ */
+public interface MathService {
+
+}
